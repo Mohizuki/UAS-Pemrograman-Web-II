1c2f188097ffcdc4a649118a9e79867c57101eb3
mohizuki <upalphb4@gmail.com>
2022-05-23 23:14:30 +0800

    layouts yield

---
+++ resources/views/layouts/listgenre.blade.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html>
+<head>
+	<meta name="viewport" content="with=device-width, initial-scale=1.0">
+	<title>Crunchii Novel</title>
+	<link rel="shortcut icon" type="image/icon" href="images/crunchiitopicon.ico" />
+	<link rel="stylesheet" href="style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    
+</head>
+<body>
+	<header>
+	<section class="header">
+		<!---- header left ---->
+		<div class="headerleft">
+			<a href="index.html"><img src="images/crunchiilogo.gif" alt=""></a>
+		</div>
+		<!---- header menu ---->
+		<div class="headermenu">
+			<nav>
+				<div class="nav-links" id="navLinks">
+				<i class="fa fa-times" onclick="hideMenu()"></i>
+				<ul>
+                    <li><a href="./">HOME</a></li>
+					<li><a href="novel">NOVEL</a></li>
+					<li><a href="genre">GENRE</a></li>
+					<li><a href="popular">POPULAR</a></li>
+					<li><a href="about">ABOUT</a></li>
+				</ul>
+				</div>	
+				<i class="fa fa-bars" onclick="showMenu()"></i>		
+			</nav>
+		</div>
+		<!---- header right ---->
+		<div class="headerright">
+			<div class="searchbutton">
+				<form action="https://www.google.com/search" method="get" class="search-bar">
+					<input type="text" placeholder="Search novel" name="s">
+					<button type="submit"><img src="images/search.png" alt=""></button>
+				</form>
+			</div>
+		</div>
+		<div class="login">
+				<a href="login"><img src="images/personlogin.png" alt=""></a>
+			</div>
+	<div class="text-box">
+		<h1>Genres</h1>
+	  <p>Find yourself your fitted genre for novels</p>
+	</div>
+	</section>
+	</header>
+	
+<!----tengah / content 1---->
+<section class="course">
+	<h1>Novels Featured</h1>
+	<p>Some of novels that are available</p>	
+	<div class="rowgenre">
+	@yield('content2')
+	</div>
+</section>
+
+	
+<!----- footer ----->	
+	
+<section class="footer">
+	<h4>About Us</h4>
+	<p>both fadil and i, naufal trying to build this website for exam's purposes</p>
+	<div class="icons">
+	<i class="fa fa-github"></i>
+	<i class="fa fa-instagram"></i>
+	<i class="fa fa-steam"></i>
+	</div>
+	<p>Made with <i class="fa fa-heart-o"></i> tears ;-;</p>
+</section>
+
+<!------JavaScript for Toggle Menu----->
+
+<script>
+	var navLinks = document.getElementById("navLinks");
+	function showMenu(){
+		navLinks.style.right = "0";
+	}
+	function hideMenu(){
+		navLinks.style.right = "-155px";
+	}
+</script>
+
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Crunchii</title>
+	<link rel="stylesheet" href="login.css">
+	<link rel="shortcut icon" type="image/icon" href="images/crunchiitopicon.ico" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+<section>
+	<div class="form-container">
+		<div class="logoform">
+		<img src="images/crunchiilogo.gif" alt="" style="width: 200px; height: 100px; top:50%; left: 50%; margin-left:20%;">
+		</div>
+		<h1>Login form</h1>
+		<form action="#" method="post">
+			<div class="control">
+				<label for="name">Name</label>
+				<input type="text" name="name" id="name">
+			</div>
+			<div class="control">
+				<label for="psw">Password</label>
+				<input type="password" name="psw" id="psw">
+			</div>
+			<span><input type="checkbox"> Remember me.</span>
+			<div class="control">
+				<input type="submit" value="login">
+			</div>
+		</form>
+		<!----- <div class="link">
+			<a href="#">Forgot password ?</a>
+		</div> ----->
+	</div>
+</section>	
+</body>
+</html>
